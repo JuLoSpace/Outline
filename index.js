89d7fe12c0db5fa6c34161ccfc9170745a58a26b
SimplyProgger <clashofclansgamerarmoo@gmail.com>
2020-11-30 21:17:14 +0500

    added initializating pages

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+const todoRoutes = require('./routes/todos');
+
+
+PORT = process.env.PORT || 3000;
+
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+app.use(todoRoutes);
+
+async function start() {
+    try {
+        app.listen(PORT, () => {
+            console.log('Server has been started!');
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+
+start();
